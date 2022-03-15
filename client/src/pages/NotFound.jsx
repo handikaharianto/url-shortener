04bd4fc339aa0bf642bd9cc2b7d11bad87345e05
Handika Harianto Ew Jong <handikaharianto01@gmail.com>
2022-03-15 21:13:53 +0800

    Complete 404 page

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -1,7 +1,20 @@
+import { Link } from "react-router-dom";
+
 function NotFound() {
   return (
-    <section>
-      <h1>404 page</h1>
+    <section className="grid place-items-center h-screen text-gray-100">
+      <div className="text-center">
+        <h1 className="font-extrabold text-7xl md:text-9xl">Oops!</h1>
+        <p className="font-medium text-3xl md:text-5xl my-8">
+          404 - Page not found!
+        </p>
+        <Link
+          to="/"
+          class="inline-block bg-violet-700 text-black-700 font-semibold py-2 px-4 rounded-md transition linear duration-300 hover:-translate-y-1 hover:scale-110"
+        >
+          BACK TO HOME
+        </Link>
+      </div>
     </section>
   );
 }
