9b3175acab6f7689342e217d633be0c481d448b5
Handika Harianto Ew Jong <handikaharianto01@gmail.com>
2022-03-15 10:25:20 +0800

    Add validation to schema

--- models/url.js
+++ models/url.js
@@ -5,6 +5,13 @@ const urlSchema = mongoose.Schema({
     type: String,
     required: [true, "URL cannot be empty!"],
     trim: true,
+    validate: {
+      validator: (v) => {
+        const regex =
+          /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-/]))?/;
+        return regex.test(v);
+      },
+    },
   },
   slug: {
     type: String,
