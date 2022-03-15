1cc70c92c16e14f512e254a57f49b4f14ffa084a
Handika Harianto Ew Jong <handikaharianto01@gmail.com>
2022-03-15 19:47:21 +0800

    Add navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import "./App.css";
 import NotFound from "./pages/NotFound";
 import Home from "./pages/Home";
+import Navbar from "./layout/Navbar";
 
 function App() {
   return (
-    <>
+    <main className="min-h-screen bg-neutral-900">
       <Router>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
-    </>
+    </main>
   );
 }
 
---
+++ client/src/layout/Navbar.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+import { CgLink } from "react-icons/cg";
+
+function Navbar() {
+  return (
+    <nav className="bg-violet-700 p-4">
+      <div className="sm:container mx-auto">
+        <h1 className="text-gray-100 text-3xl font-semibold">
+          <Link to="/" className="flex items-center">
+            <CgLink className="mr-1" />
+            URL Shortener
+          </Link>
+        </h1>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
